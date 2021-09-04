d4caf7d4e07b29d8f2174505dab1acd3525771c0
hyunchul <hyuncil42@gmail.com>
2021-09-04 12:11:36 +0900

    1.A개발자가 B에게 주석을 알리기 위해 작성함.

--- lib/models/youtube_video_result.dart
+++ lib/models/youtube_video_result.dart
@@ -14,5 +14,6 @@ class YoutubeVideoResult {
        totalResults: json['pageInfo']['totalResults'],
        resultPerPage: json['pageInfo']['resultPerPage'],
        items: List<Video>.from(json['items'].map((data)=>Video.fromJson(data))));
-
+ // 리스트 타입은 위와 같이 맵으로 돌려준다
 }
+
